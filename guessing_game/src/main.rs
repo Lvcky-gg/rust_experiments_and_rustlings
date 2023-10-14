3fb2fe3e594850082e9f4e3f8ae645e060bcffb7
John O'Donnell <matadorcrab@gmail.com>
2023-10-14 16:06:43 -0500

    init

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+use::std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop{
+    println!("Please input a guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {guess}");
+
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_)=> continue,
+    };
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => {
+            println!("You win");
+            break;
+        },
+    }
+}
+}
